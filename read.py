c68c89385e7d2240c6f3809bc323e536b42d7230
sudo-flech <alejo.flechas18@gmail.com>
2025-01-15 19:17:31 -0500

    13:11

--- read.py
+++ read.py
@@ -11,10 +11,19 @@ cv.waitKey(0) #Delay hasta que se presione una letra
 
 
 
+
+
 # Reading Videos
+'''
 capture = cv.VideoCapture('Resources/Videos/dog.mp4') #El argumento 0 es la webcam
 while True:
     isTrue, frame = capture.read()
+    
     cv.imshow('Video', frame)
+    
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
 
+capture.release()
 cv.waitKey(0)
+'''
